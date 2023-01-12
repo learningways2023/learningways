48c5980cc076f5a16d38d8249c83f7e716570204
learningways2023 <info.learningways@gmail.com>
2023-01-12 22:14:39 +0530

    jana2 commit

--- next.config.js
+++ next.config.js
@@ -5,12 +5,16 @@ const nextConfig = {
 
 module.exports = {
   nextConfig,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "icons8.com",
-        
       },
     ],
   },
