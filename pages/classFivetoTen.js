4b448ad32db83fa682730594b1120eb9f8662ee0
learningways2023 <info.learningways@gmail.com>
2023-01-25 09:58:02 +0530

    jana 250957

--- pages/classFivetoTen.js
+++ pages/classFivetoTen.js
@@ -6,13 +6,15 @@ import PremiumCourseCard from "../components/premiumCourseCard";
 import Footer from "../components/footer";
 import face from "../public/facebook.png";
 import insta from "../public/insta.png";
+import { useRouter } from "next/router";
 import linked from "../public/linkedin.png";
 import { Player, Controls } from "@lottiefiles/react-lottie-player";
-
+import UpcommingCard from "../components/upcommingCard";
 import twitter from "../public/twitter.png";
 import dbms from "../public/dbms.png";
 import john from "../public/john.png";
 const ClassFivetoTen = () => {
+  const router = useRouter();
   const classes =
     "border-2 border-[#085464] text-[#085464] text-base font-medium w-full h-full rounded-md focus:ring-[#085464] focus:border-[#085464]  block py-2.5 lg:px-5 xl:px-5 px-12 ";
   const FormHeader = (props) => (
@@ -26,49 +28,89 @@ const ClassFivetoTen = () => {
       <div className="p-4 min-h-[50vh]">
         <div className="py-9 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
           <div className="w-full h-full flex justify-center items-center">
-            <div className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]   border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal">
+            <div
+              className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]   border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal"
+              onClick={function handleClick() {
+                router.push("/classFivetoTen/#coding");
+              }}
+            >
               Coding Course
             </div>
           </div>
           <div className=" w-full h-full flex justify-center items-center">
-            <div className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal">
+            <div
+              className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal"
+              onClick={function handleClick() {
+                router.push("/classFivetoTen");
+              }}
+            >
               NCERT
             </div>
           </div>
           <div className=" w-full h-full flex justify-center items-center">
-            <div className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal">
+            <div
+              className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal"
+              onClick={function handleClick() {
+                router.push("/classFivetoTen/#private_tutor");
+              }}
+            >
               Private Tutoring
             </div>
           </div>
           <div className=" w-full h-full flex justify-center items-center">
-            <div className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal">
+            <div
+              className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal"
+              onClick={function handleClick() {
+                router.push("/classFivetoTen/#personality_developement");
+              }}
+            >
               Personality Development
             </div>
           </div>
           <div className=" w-full h-full flex justify-center items-center">
-            <div className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal">
+            <div
+              className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal"
+              onClick={function handleClick() {
+                router.push("/classFivetoTen/#quiz");
+              }}
+            >
               Quiz Section
             </div>
           </div>
           <div className=" w-full h-full flex justify-center items-center">
-            <div className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal">
+            <div
+              className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal"
+              onClick={function handleClick() {
+                router.push("/classFivetoTen/#join");
+              }}
+            >
               Join Community
             </div>
           </div>
           <div className=" w-full h-full flex justify-center items-center">
-            <div className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]   border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal">
+            <div
+              className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]   border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal"
+              onClick={function handleClick() {
+                router.push("/classFivetoTen/#materials");
+              }}
+            >
               Reading Materials
             </div>
           </div>
           <div className=" w-full h-full flex justify-center items-center">
-            <div className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal">
+            <div
+              className="w-[150px] min-h-[70px] lg:w-[200px] xl:h-[90px]     border border-1 hover:text-white border-[#085464] rounded-lg bg-white hover:bg-[#085464]  text-[#085464] cursor-pointer text-center flex justify-center items-center font-normal"
+              onClick={function handleClick() {
+                router.push("/classFivetoTen/#summer_camp");
+              }}
+            >
               Summer Camp
             </div>
           </div>
         </div>
       </div>
       <div className="p-4 py-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
-        <div className="text-[#085464] font-medium flex  items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
+        <div id="coding" className="text-[#085464] font-medium flex  items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
           Coding Course
         </div>
         <div className="p-4 flex-row md:flex lg:flex xl:flex justify-center  md:justify-between xl:justify-between lg:justify-between space-x-4 md:items-center ">
@@ -192,7 +234,7 @@ const ClassFivetoTen = () => {
           </div>
         </div>
       </div>
-      <div className="p-4 pt-9 pb-4  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+      <div id="private_tutor" className="p-4 pt-9 pb-4  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
         <div className="text-[#085464] font-medium flex  items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
           Private Tutor
         </div>
@@ -246,7 +288,7 @@ const ClassFivetoTen = () => {
         </div>
       </div>
       <div className="p-4 py-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
-        <div className="text-[#085464] font-medium flex  items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
+        <div id="personality_developement" className="text-[#085464] font-medium flex  items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
           Personality Development
         </div>
         <div className="p-4 flex-row md:flex lg:flex xl:flex justify-center  md:justify-between xl:justify-between lg:justify-between space-x-4 md:items-center ">
@@ -371,7 +413,7 @@ const ClassFivetoTen = () => {
         </div>
       </div>
 
-      <div className="text-[#085464] p-4 py-9 font-medium flex justify-center  items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
+      <div id="quiz" className="text-[#085464] p-4 py-9 font-medium flex justify-center  items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
         Quiz Section
       </div>
 
@@ -477,7 +519,7 @@ const ClassFivetoTen = () => {
           </div>
         </div>
       </div>
-      <div className="w-full min-h-[60vh] bg-[#085464] ">
+      <div id="join" className="w-full min-h-[60vh] bg-[#085464] ">
         <div className="text-[#ffff] py-4 font-medium flex justify-center items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
           Join Community
         </div>
@@ -503,9 +545,84 @@ const ClassFivetoTen = () => {
           </div>
         </div>
       </div>
-
+      <div className="p-4 pt-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+        <div className="text-[#085464] font-medium flex  items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
+          Upcomming Events
+        </div>
+        <div className="p-4 flex-row md:flex lg:flex xl:flex justify-center  md:justify-between xl:justify-between lg:justify-between space-x-4 md:items-center ">
+          <div className="py-2 md:py-0 lg:py-0 xl:py-0 flex justify-center items-center ">
+            <div className="w-[130px] h-[50px] bg-[#085464] rounded-xl flex justify-center items-center cursor-pointer">
+              <div>
+                <Image src={filter} width={20} height={20} />
+              </div>
+              <div className="pl-3 text-[20px] text-white">Grades</div>
+            </div>
+          </div>
+          <div className="py-2 md:py-0 lg:py-0 xl:py-0 flex justify-center items-center">
+            <div className="min-w-[130px] h-[50px] p-2 bg-[#085464] rounded-xl flex justify-center items-center cursor-pointer">
+              <div>
+                <Image src={filter} width={20} height={20} />
+              </div>
+              <div className="pl-3 text-[20px] text-white">Search Events</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 w-full flex flex-wrap">
+        <UpcommingCard
+          img={dbms}
+          tittle="International Yoga Day"
+          s1="Learn the basics of digital marketing, including web design, search engine optimization, social media, and online advertising."
+          s2="Understand the importance of digital marketing in today's business environment"
+          array={[
+            "Understand the importance of digital marketing in today's business environment",
+            "Learn the different channels and tactics used in digital marketing",
+            "Discover how to create a digital marketing strategy",
+            "Learn the basics of website design and optimization",
+            "Understand the role of search engine optimization (SEO) in digital marketing",
+            "Learn how to use social media for business",
+            "Understand the basics of online advertising, including pay-per-click and display advertising",
+            "Learn how to measure and analyze the effectiveness of your digital marketing efforts",
+            "Discover how to stay up-to-date with the latest digital marketing trends and best practices",
+          ]}
+        />
+        <UpcommingCard
+          img={dbms}
+          tittle="International Yoga Day"
+          s1="Learn the basics of digital marketing, including web design, search engine optimization, social media, and online advertising."
+          s2="Understand the importance of digital marketing in today's business environment"
+          array={[
+            "Understand the importance of digital marketing in today's business environment",
+            "Learn the different channels and tactics used in digital marketing",
+            "Discover how to create a digital marketing strategy",
+            "Learn the basics of website design and optimization",
+            "Understand the role of search engine optimization (SEO) in digital marketing",
+            "Learn how to use social media for business",
+            "Understand the basics of online advertising, including pay-per-click and display advertising",
+            "Learn how to measure and analyze the effectiveness of your digital marketing efforts",
+            "Discover how to stay up-to-date with the latest digital marketing trends and best practices",
+          ]}
+        />
+        <UpcommingCard
+          img={dbms}
+          tittle="International Yoga Day"
+          s1="Learn the basics of digital marketing, including web design, search engine optimization, social media, and online advertising."
+          s2="Understand the importance of digital marketing in today's business environment"
+          array={[
+            "Understand the importance of digital marketing in today's business environment",
+            "Learn the different channels and tactics used in digital marketing",
+            "Discover how to create a digital marketing strategy",
+            "Learn the basics of website design and optimization",
+            "Understand the role of search engine optimization (SEO) in digital marketing",
+            "Learn how to use social media for business",
+            "Understand the basics of online advertising, including pay-per-click and display advertising",
+            "Learn how to measure and analyze the effectiveness of your digital marketing efforts",
+            "Discover how to stay up-to-date with the latest digital marketing trends and best practices",
+          ]}
+        />
+      </div>
       <div className="pt-9"></div>
-      <div className="p-4 pt-9 pb-3 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1">
+      <div id="summer_camp" className="p-4 pt-9 pb-3 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1">
         <div className="text-[#085464]  flex-row  items-center text-center  lg:text-left xl:text-left">
           <div className="xl:text-[42px] font-medium lg:text-[32px] sm:text-[32px] text-[20px]">
             Summer Camps
@@ -613,7 +730,7 @@ const ClassFivetoTen = () => {
                 <div className="pl-5">
                   <div className="lg:text-lg md:text-lg text-base  font-semibold">
                     Danielle Dodder | Parent, Naples, FL
-                  </div>
+                    personality_developement        </div>
                 </div>
               </div>
             </div>
@@ -731,7 +848,7 @@ const ClassFivetoTen = () => {
       </div>
       <div className="pt-7"></div>
       <div className="text-[#085464] p-4 py-9 font-medium flex justify-center  items-center text-center xl:text-[42px] lg:text-[32px] sm:text-[32px] text-[20px] lg:text-left xl:text-left">
-       Feedback: Your Ward deserves best & we are on it .
+        Feedback: Your Ward deserves best & we are on it .
       </div>
       <Footer />
     </div>
