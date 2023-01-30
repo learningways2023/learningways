13776beacd786e3ff8c99e7a766eda49e21a4235
learningways2023 <info.learningways@gmail.com>
2023-01-30 19:20:04 +0530

    jana 301920

--- components/navbar.js
+++ components/navbar.js
@@ -81,7 +81,7 @@ export default function Navbar() {
                       <div
                         className="flex justify-center items-center py-2 cursor-pointer hover:bg-white hover:text-[#085464]"
                         onClick={function handleLogo() {
-                          router.push("/");
+                          router.push("/NCERT_Solutions");
                           setNavbar(false);
                         }}
                       >
