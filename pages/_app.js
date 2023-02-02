424a9406bb5a82d908a88196753201e476fb53ab
learningways2023 <info.learningways@gmail.com>
2023-02-02 13:44:31 +0530

    jana 021344

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import { Roboto } from "@next/font/google";
+import { Halant } from "@next/font/google";
 import { ParallaxProvider } from "react-scroll-parallax";
 import "../styles/globals.css";
 import Head from "next/head";
-const inter = Roboto({ weight: "400", subsets: ["latin"] });
+const inter = Halant({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
