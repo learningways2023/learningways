fcaa5ffd5b62fca230bc3eface0bbc9e4133ecb7
learningways2023 <info.learningways@gmail.com>
2023-02-11 19:07:40 +0530

    jana 022117

---
+++ components/sidebar.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Disclosure } from "@headlessui/react";
+import Image from "next/image";
+import logo from "../public/logo2.png";
+import home from "../public/home.png";
+import dash from "../public/dash.png";
+import enroll from "../public/enroll.png";
+import profile from "../public/profile.png";
+import logout from "../public/logout.png";
+import { useRouter } from "next/router";
+import { userAgentFromString } from "next/server";
+function SideNavbar(props) {
+  const router = useRouter();
+  return (
+    <div>
+      <Disclosure as="nav" className="h-10 ">
+        <Disclosure.Button className="md:hidden absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-600    focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="black"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clip-rule="evenodd"
+              fill-rule="evenodd"
+              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+            ></path>
+          </svg>
+        </Disclosure.Button>
+        <div className=" p-2 w-full min-h-screen  bg-gradient-to-r from-blue-500 to-cyan-500 z-20 relative  md:top-0  -left-[500px] md:left-0 md:w-80  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+          <div className="flex justify-between items-center py-4 px-4 w-full">
+            <div>
+              <Image src={logo} height={70} width={250} />
+            </div>
+            <div className="md:hidden flex justify-end items-center pl-9">
+              <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M19.2803 1.78033C19.5732 1.48744 19.5732 1.01256 19.2803 0.71967C18.9875 0.426777 18.5126 0.426777 18.2197 0.71967L19.2803 1.78033ZM0.71967 18.2197C0.426777 18.5126 0.426777 18.9875 0.71967 19.2803C1.01256 19.5732 1.48744 19.5732 1.78033 19.2803L0.71967 18.2197ZM1.78037 0.71967C1.48747 0.426777 1.0126 0.426777 0.719707 0.71967C0.426813 1.01256 0.426813 1.48744 0.719707 1.78033L1.78037 0.71967ZM18.2197 19.2803C18.5126 19.5732 18.9875 19.5732 19.2804 19.2803C19.5733 18.9875 19.5733 18.5126 19.2804 18.2197L18.2197 19.2803ZM18.2197 0.71967L0.71967 18.2197L1.78033 19.2803L19.2803 1.78033L18.2197 0.71967ZM0.719707 1.78033L18.2197 19.2803L19.2804 18.2197L1.78037 0.71967L0.719707 1.78033Z"
+                  fill="#ffff"
+                  fill-opacity="1"
+                />
+              </svg>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-between ">
+            <div className="flex flex-col items-center justify-between space-y-5">
+              <div className="text-xl text-black flex w-full  justify-start items-center font-semibold py-4  cursor-pointer " onClick={function handleLinkl(){
+                router.push("/")
+              }}>
+                <Image src={home} width={30} height={30} />
+                <div className="pl-4">Landing Page</div>
+              </div>
+              <div className="text-xl text-black flex w-full  justify-start items-center font-semibold py-4 cursor-pointer  "onClick={function handleLinkl(){
+                router.push("/dashboard")
+              }}>
+                <Image src={dash} width={30} height={30} />
+                <div className="pl-4">Dashboard</div>
+              </div>
+              <div className="text-xl text-black flex w-full  justify-start items-center font-semibold py-4  cursor-pointer ">
+                <Image src={enroll} width={40} height={30} />
+                <div className="pl-2">Enrolled Courses</div>
+              </div>
+              <div className="text-xl text-black flex w-full  justify-start items-center font-semibold py-4  cursor-pointer ">
+                <div className="pl-4"></div>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-between pt-20 space-y-10">
+              <div className="text-xl text-black flex w-full   justify-center items-center font-semibold py-4  cursor-pointer ">
+                <Image src={profile} width={40} height={30} />
+                <div className="pl-2"> Profile</div>
+              </div>
+
+              <div className="">
+                <div className="border-2 text-white rounded items-center flex justify-center w-[300px] py-2 text-base  cursor-pointer font-semibold bg-black border-black duration-150">
+                  <button className="pr-5">Logout</button>
+                  <Image src={logout} width={30} height={30} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Disclosure>
+    </div>
+  );
+}
+
+export default SideNavbar;
--- pages/new.js
+++ pages/new.js
@@ -1,24 +1,29 @@
 import React from "react";
-import Image from "next/image";
-import ment from "../public/john.png";
+import SideNavbar from "../components/sidebar";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
 const New = () => {
+  Chart.register(ArcElement);
+  const data = {
+    labels: ["Red", "Green", "Yellow"],
+    datasets: [
+      {
+        data: [300, 50, 100],
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+      },
+    ],
+  };
   return (
-    <div className="bdy">
-      <div className="container">
-        <div className="card">
-          <div className="imgBx">
-            <Image src={ment} width={150} height={150} />
+    <div className="md:flex">
+      <SideNavbar />
+      <div className=" min-h-[95vh] m-0 mt-4 md:m-4 w-[100vw] flex justify-center items-center bg-gradient-to-r from-blue-500 to-cyan-500 rounded-md">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6">
+          <div className="w-full h-full flex  justify-center items-center">
+            <Doughnut data={data} width={400} height={400} />
           </div>
-          <div className="contentBx">
-            <h2>Design</h2>
-            <p>
-              Welcome to our comprehensive skill development platform. Discover
-              a wide range of courses and resources to help you learn new
-              skills, improve existing ones and advance your career. With our
-              expert-led training, interactive practice exercises and real-world
-              examples, you'll have everything you need to succeed in your
-              professional and personal life.
-            </p>
+          <div className="w-full h-full flex  justify-center items-center">
+            <Doughnut data={data} width={400} height={400} />
           </div>
         </div>
       </div>
